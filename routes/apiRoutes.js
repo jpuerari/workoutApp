6987e446aef2d6c91c827c72690af590265b241e
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2020-04-02 19:35:42 -0400

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,5 +18,47 @@ router.get("/api/workouts", function(req, res){
 //findOneAndUpdate or findByIdAndUpdate or UpdateOne
 //db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true })
 
+router.put("/workouts/:id", (req, res) => {
+    console.log(req.params, req.body)
+    Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+          exercises: req.body
+        }
+      }, {
+        new: true,
+        runValidators: true
+      })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        console.log(err)
+        res.status.json(err);
+      });
+  });
+  
+  router.get("/workouts", (req, res) => {
+    console.log("Here")
+    Workout.find({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        console.log(err)
+        res.status.json(err);
+      });
+  })
+  
+  router.get("/workouts/range", (req, res) => {
+    Workout.find({})
+      .limit(10)
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status.json(err);
+      });
+  })
+
 
 module.exports = router;
