5f733ef3a672ca1bb56221596e73f74b1f4d07a4
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2020-04-01 22:15:30 -0400

    updated server.js

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-
-
-
-
-
-
+const routes = require('./routes');
+app.use(routes)
 
 
 //port
